5c46e29cfbda2e9dc1e4f45075d9f32c3fb0ca2e
Daniel Tsutomu Obara <daniel.tsutomu@hotmail.com>
2020-05-26 22:28:04 -0300

    Finish 8 kyu Welcome

--- HowManyPalindromes/index.js
+++ HowManyPalindromes/index.js
@@ -1,3 +1,13 @@
 function howManyPalindromes(text) {
   return 0;
 }
+
+const factorial = (n) => {
+    var result = 1;
+    for(var i = n; i >= n; i--){
+     result = n * i;
+    }
+    return result;
+};
+
+console.log(factorial(5));
---
+++ Welcome/index.js
@@ -0,0 +1,30 @@
+const languages = 
+  {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+
+
+function greet(language) {
+	const restult =
+	languages[language] ? languages[language] : "Welcome";
+	
+  return restult;
+}
+
+console.log("greet -> greet", greet("spanish"));
